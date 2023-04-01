481752da0d9fd64db555f486670bb07b5bf06bdd
Alohadancemeow <nawongsak@gmail.com>
2023-04-01 21:38:06 +0700

    🍀 posts: add timeline

--- components/SearchPost.tsx
+++ components/SearchPost.tsx
@@ -1,41 +1,42 @@
-import React from 'react'
-import { Divider, UnstyledButton, Box, Kbd } from '@mantine/core'
-import { Search } from 'tabler-icons-react';
-import { openSpotlight } from '@mantine/spotlight'
+import React from "react";
+import { Divider, UnstyledButton, Box, Kbd } from "@mantine/core";
+import { Search } from "tabler-icons-react";
+import { openSpotlight } from "@mantine/spotlight";
 
-
-type Props = {}
+type Props = {};
 
 const SearchPost = (props: Props) => {
-    return (
-        <Divider
-            // size="sm"
-            my="xs"
-            variant="solid"
-            labelPosition="center"
-            label={
-                <UnstyledButton
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        // fontWeight: '500'
-                    }}
-                    onClick={() => openSpotlight()}
-                >
-                    <Search size={28} />
-                    <Box ml={5} mr={10}>Search</Box>
-                    <Kbd>⌘</Kbd>
-                    <span style={{ margin: '0 5px' }}>+</span>
-                    <Kbd>S</Kbd>
-                    <span style={{ margin: '0 10px' }}>/</span>
-                    <Kbd>Ctrl</Kbd>
-                    <span style={{ margin: '0 5px' }}>+</span>
-                    <Kbd>S</Kbd>
-                </UnstyledButton>
-            }
-        />
-    )
-}
+  return (
+    <Divider
+      // size="sm"
+      my="xs"
+      variant="solid"
+      labelPosition="center"
+      label={
+        <UnstyledButton
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            // fontWeight: '500'
+          }}
+          onClick={() => openSpotlight()}
+        >
+          <Search size={28} />
+          <Box ml={5} mr={10}>
+            Search
+          </Box>
+          <Kbd>⌘</Kbd>
+          <span style={{ margin: "0 5px" }}>+</span>
+          <Kbd>S</Kbd>
+          <span style={{ margin: "0 10px" }}>/</span>
+          <Kbd>Ctrl</Kbd>
+          <span style={{ margin: "0 5px" }}>+</span>
+          <Kbd>S</Kbd>
+        </UnstyledButton>
+      }
+    />
+  );
+};
 
-export default SearchPost
+export default SearchPost;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,54 +1,69 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { MantineProvider, ColorSchemeProvider, ColorScheme, Space } from '@mantine/core';
-import { useHotkeys, useLocalStorage } from '@mantine/hooks';
+import { AppProps } from "next/app";
+import Head from "next/head";
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+  Space,
+} from "@mantine/core";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
 
-import GlobalStyles from 'styles/GlobalStyles';
-import { useState } from 'react';
-import Player from 'components/Player';
+import GlobalStyles from "styles/GlobalStyles";
+import { useState } from "react";
+import Player from "components/Player";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: 'mantine-color-scheme',
-    defaultValue: 'light',
+    key: "mantine-color-scheme",
+    defaultValue: "light",
     getInitialValueInEffect: true,
   });
 
   const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
-  useHotkeys([['mod + D', () => toggleColorScheme()]]);
+  useHotkeys([["mod + D", () => toggleColorScheme()]]);
 
   const [opened, setOpened] = useState<boolean>(false);
-  useHotkeys([['mod + shift + L', () => setOpened((o) => !o)]]);
+  useHotkeys([["mod + shift + L", () => setOpened((o) => !o)]]);
 
   return (
     <>
       <Head>
         {/* <title>Personal Home</title> */}
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
         <link rel="icon" href="/astonished.svg" />
       </Head>
 
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
         <MantineProvider
           withGlobalStyles
           withNormalizeCSS
           theme={{
             /** Put your mantine theme override here */
             colorScheme: colorScheme,
-            primaryColor: 'orange',
+            primaryColor: "orange",
             fontFamily: ["'JetBrains Mono', 'Bai Jamjuree'"],
 
             headings: {
-              fontFamily: 'JetBrains Mono',
-              fontWeight: '500',
+              fontFamily: "JetBrains Mono",
+              fontWeight: "500",
               sizes: {
-                h2: 24,
-                h3: 18
-              }
+                h2: {
+                  fontSize: 24,
+                },
+                h3: {
+                  fontSize: 18,
+                },
+              },
             },
             breakpoints: {
               xs: 500,
