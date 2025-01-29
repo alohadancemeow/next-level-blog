806c10073fcf1f51d5669d673abf144dff80ff32
Alohadancemeow <nawongsak@gmail.com>
2025-01-29 19:54:00 +0700

    🐛 fixed: about and note page

--- app/note/layout.tsx
+++ app/note/layout.tsx
@@ -1,15 +1,18 @@
 import Layout from "@/components/layout/Layout";
 import Menu from "@/components/layout/Menu";
 import PageLayout from "@/components/layout/PageLayout";
+import MantineProviders from "@/components/providers/MantineProviders";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Layout>
-      <PageLayout>
-        <Menu title="Liberation Notes" />
-        {children}
-      </PageLayout>
-    </Layout>
+    <MantineProviders>
+      <Layout>
+        <PageLayout>
+          <Menu title="Liberation Notes" />
+          {children}
+        </PageLayout>
+      </Layout>
+    </MantineProviders>
   );
 };
 
