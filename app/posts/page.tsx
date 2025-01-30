d5e5148073a4a8cae3a2de29f556d7ada447082a
Alohadancemeow <nawongsak@gmail.com>
2025-01-30 20:59:40 +0700

    🐛 fixed: add dynamic export to multiple pages and enhance RelatedPosts component layout

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { Metadata } from "next";
 import { getAllPosts } from "@/actions/notion";
 
--- components/contents/RelatedPosts.tsx
+++ components/contents/RelatedPosts.tsx
@@ -4,6 +4,8 @@ import useGetRelatedPosts from "@/hooks/use-get-related-posts";
 import { PageDataSchemaType } from "@/types";
 import { Center, Text } from "@mantine/core";
 import MorePost from "./MorePost";
+import useLayoutStore from "@/hooks/use-layout-store";
+import PostCardFlex from "@/app/posts/components/PostCardFlex";
 
 type Props = {
   posts: PageDataSchemaType[];
@@ -11,24 +13,36 @@ type Props = {
 };
 
 const RelatedPosts = ({ postData, posts }: Props) => {
+  const { isGrid } = useLayoutStore();
   const { relatedPosts } = useGetRelatedPosts({
     posts,
     postTags: postData.tags,
     postId: postData.id,
   });
 
+  if (relatedPosts.length === 0)
+    return (
+      <Center mt={30}>
+        <Text>No post matched... 😕</Text>
+      </Center>
+    );
+
   return (
     <div>
-      {relatedPosts.length !== 0 ? (
+      {isGrid && (
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {relatedPosts.map((post) => (
             <MorePost key={post.id} post={post} />
           ))}
         </div>
-      ) : (
-        <Center mt={30}>
-          <Text>No post matched... 😕</Text>
-        </Center>
+      )}
+
+      {!isGrid && (
+        <div className="flex flex-col gap-1">
+          {relatedPosts.map((item) => (
+            <PostCardFlex key={item.id} post={item} />
+          ))}
+        </div>
       )}
     </div>
   );
